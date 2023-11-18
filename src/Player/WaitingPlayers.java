6e5d76b33f3e9e1797b30a52171f827b178e4cf9
DavidSan <84092328+DavidSan9896@users.noreply.github.com>
2023-11-18 18:29:07 -0500

    Pantalla de juego

--- src/Player/WaitingPlayers.java
+++ src/Player/WaitingPlayers.java
@@ -11,28 +11,40 @@ public class WaitingPlayers extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JLabel playersLabel;
     private JLabel waitingMessage;
+    private JPanel waitingPlayerPanel;
 
     public WaitingPlayers() {
        
-        setTitle("Esperando Jugadores");
+        setTitle("BlackJack");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 200);
         setLocationRelativeTo(null); 
         setLayout(new BorderLayout());
+        
+        waitingPlayerPanel = new JPanel();
 
         playersLabel = new JLabel("Jugadores Conectados: 0", SwingConstants.CENTER);
         playersLabel.setFont(new Font("Arial", Font.PLAIN, 14));
-        add(playersLabel, BorderLayout.NORTH);
+        waitingPlayerPanel.add(playersLabel, BorderLayout.NORTH);
 
         waitingMessage = new JLabel("Esperando jugadores", SwingConstants.CENTER);
         waitingMessage.setFont(new Font("Arial", Font.BOLD, 18));
-        add(waitingMessage, BorderLayout.CENTER);
+        waitingPlayerPanel.add(waitingMessage, BorderLayout.CENTER);
 
+        this.add(waitingPlayerPanel, BorderLayout.CENTER);
+        
         setVisible(true);
     }
 
     public void setPlayersCount(String count) {
         playersLabel.setText("Jugadores Conectados: " + count);
+        if (count.equals("3")) {
+			waitingPlayerPanel.removeAll();
+			JButton yap = new JButton("Panel de juego");
+			waitingPlayerPanel.add(yap, BorderLayout.CENTER);
+			waitingPlayerPanel.revalidate();
+			waitingPlayerPanel.repaint();
+		}
     }
 
     public static void main(String[] args) {
@@ -40,5 +52,6 @@ public class WaitingPlayers extends JFrame {
         Player player = new Player(waitingPlayersFrame);
         player.start();
     }
+    
 }
 
